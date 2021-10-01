0d448e92a6103e482689cc7addfdb90ed32dd832
Julio12-prog <u202017572@upc.edu.pe>
2021-10-01 13:28:39 -0500

    feat: list of items in course
    list of items in course and update db.json

--- src/list-courses/pages/list-courses.vue
+++ src/list-courses/pages/list-courses.vue
@@ -6,9 +6,101 @@
         <v-card width="800px" class="mx-auto">
           <v-card-title class="white--text blue accent-4">{{course.name}}</v-card-title>
           <v-card-text class="black--text" align="left">{{course.description}}</v-card-text>
-          <v-card-actions>
-            <v-btn text class="black white--text">see details</v-btn>
-          </v-card-actions>
+          <v-dialog max-width="1500px" overlay-color="white" overlay-opacity="50">
+            <template v-slot:activator="{on, attrs}">
+              <v-btn text color="indigo accent-4" datk v-on="on" v-bind="attrs" @click="getCourse(course.id)">Learn More</v-btn>
+            </template>
+            <v-container>
+              <v-row>
+                <!--  -->
+                <v-col cols="12" md="12"><v-spacer></v-spacer></v-col>
+                <v-col cols="12" sm="6" md="8">
+                  <h1 class="font-weight-bold">Información del docente</h1>
+                  <v-col cols="12" md="12"><v-spacer></v-spacer></v-col>
+                  <v-card class="mx-auto" max-width="1010">
+                    <v-card-title class="text-h5 mb-1 teacher-profile-name font-weight-bold white--text">
+                      Curso: {{courseSelected.name}}
+                    </v-card-title>
+                    <v-list-item three-line>
+                      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+                      <v-list-item-content>
+                        <v-list-item-content>
+                          Información detalla del curso:<br>
+                          Description: {{courseSelected.description}} <br>
+                        </v-list-item-content>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-card>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <!-- Unidades -->
+                <v-col cols="8">
+                  <v-container>
+                    <h5 class="unit-bar-title">Items</h5>
+                      <div v-for="item in courseSelected.items" :key="item.id">
+                        <v-card class="mx-auto" max-width="344">
+
+                          <v-card-title>{{ item.title }}</v-card-title>
+
+                          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+
+                          <v-card-actions>
+                            <v-btn color="orange lighten-2" v-bind:href="item.conexion">Explore</v-btn>
+
+                            <v-spacer></v-spacer>
+
+                            <v-btn icon @click="show = !show">
+                              <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                            </v-btn>
+                          </v-card-actions>
+
+                          <v-expand-transition>
+                            <div v-show="show">
+                              <v-divider></v-divider>
+
+                              <v-card-text>
+                                {{ item.resume }}
+                              </v-card-text>
+                            </div>
+                          </v-expand-transition>
+                        </v-card>
+                      </div>
+
+                  </v-container>
+
+                </v-col>
+                <!-- Competencias -->
+                <v-col cols="4">
+                  <v-card class="mx-auto" max-width="220">
+                    <v-card-title class="justify-center white--text title-competences">
+                      <h4>Competencias</h4>
+                    </v-card-title>
+                    <v-card-actions>
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 1</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 2</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 3</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 4</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 5</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 6</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 7</v-btn>
+                          <v-btn rounded x-small class="white--text mt-1 mb-1" color="black accent-4">Competencia 8</v-btn>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+
+
+          </v-dialog>
+
+
+
         </v-card>
       </v-col>
     </div>
@@ -21,7 +113,19 @@ import ListCoursesService from '../services/list-courses.service'
 export default {
   name: "list-courses",
   data: () => ({
+    show: false,
     listCourses: [],
+    courseSelected: {
+      id: '',
+      name: '',
+      description: '',
+      items: [{
+        ide: '',
+        title: '',
+        resume: '',
+        conexion: ''
+      }]
+    }
 
   }),
   created() {
@@ -39,13 +143,45 @@ export default {
       return {
         id: course.id,
         name: course.name,
-        description: course.description
+        description: course.description,
+        items: course.items
       };
     },
+    getCourse(id){
+      ListCoursesService.getById(id)
+        .then((response) => {
+          this.courseSelected = response.data;
+          console.log(response.data);
+        })
+    }
   }
 }
 </script>
 
 <style scoped>
+v-container {
+  background-color: #ECEBE9;
+}
 
+/* Teacher's profile */
+.teacher-profile-name {
+  background-color: #2C53C9;
+}
+
+
+/* Unity bars*/
+.unit-bar-title {
+  font-weight: bold;
+}
+.unit-bar-text {
+  font-size: 14px;
+}
+.unit-bar-value {
+  font-weight: bold;
+}
+
+/* Competences Card */
+.title-competences {
+  background-color: #2C53C9;
+}
 </style>
